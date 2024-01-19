39eb65a3a1b4c6908eb883d4ef20c38bc2a13803
Onur-Yılmaz <onuryilmaz@trair.com.tr>
2024-01-19 05:21:06 +0300

    remove console.log

--- src/graphql/Client.tsx
+++ src/graphql/Client.tsx
@@ -9,12 +9,4 @@ const client = new ApolloClient({
 client.query({
     query: ALL_PEOPLE_QUERY,
   })
-    .then((result) => {
-      console.log(result);
-    })
-    .catch((error) => {
-      console.error('Error fetching data:', error);
-    });
-  
-
 export default client;
