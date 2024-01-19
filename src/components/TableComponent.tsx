d5809734ca20d1cd1faddc9bb70ca2d4158ee0a7
Onur-Yılmaz <onuryilmaz@trair.com.tr>
2024-01-19 03:04:36 +0300

    mb responsive

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
 const TableComponent = ({ filteredPeople }: { filteredPeople: Array<{ name: string, gender: string, eyeColor: string }> }) => {
   return (
-    <div className="mb-4">
+    <div className="mb-4 overflow-x-auto">
       <table className="min-w-full border border-gray-800">
         <thead>
           <tr className="bg-gray-900 text-white">
@@ -12,10 +10,14 @@ const TableComponent = ({ filteredPeople }: { filteredPeople: Array<{ name: stri
           </tr>
         </thead>
         <tbody>
-          {filteredPeople.map((person: { name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; gender: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; eyeColor: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }, index: React.Key | null | undefined) => (
-            <tr key={index} className={Number(index) % 2 === 0 ? 'bg-gray-800' : 'bg-gray-700'}>
-              <td className="py-2 px-4">{person.name}</td>
-              <td className="py-2 px-4">{person.gender}</td>
+          {filteredPeople.map((person: { name: string, gender: string, eyeColor: string }, index: number) => (
+            <tr key={index} className={index % 2 === 0 ? 'bg-gray-800' : 'bg-gray-700'}>
+              <td className="py-2 px-4 whitespace-nowrap overflow-hidden overflow-ellipsis">
+                {person.name}
+              </td>
+              <td className="py-2 px-4 whitespace-nowrap overflow-hidden overflow-ellipsis">
+                {person.gender}
+              </td>
               <td className="py-2 px-4">{person.eyeColor}</td>
             </tr>
           ))}
